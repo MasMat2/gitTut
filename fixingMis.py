a46521798b3e32d0f9f85b4cc229653308f73368
Max <maaxx.360@gmail.com>
2018-06-28 15:31:54 -0500

    Fundamentals of git

---
+++ fixingMis.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    pass
+
+def substract(x, y):
+    pass
+
+def multiple(x, y):
+    pass
+
+def devide(x, y):
+    pass
---
+++ fundamentals.py
@@ -0,0 +1,45 @@
+""""
+git --version
+
+git config --global user.name ""
+git config --global user.mail ""
+
+git config --list
+
+
+git help config
+git config --help
+
+GETTING STARTED
+
+git init
+ls -la
+rm -rf .git
+
+git status
+
+touch .gitingnore
+(Add file that won't be added)
+.DS_Store
+.project
+*.pyc
+
+ADD FILES TO STAGING AREA
+
+git add -A
+git status
+git add file
+
+REMOVE FILES FROM STAGING AREA
+
+git reset
+
+git status
+
+COMMITING FILES
+
+git add -A
+git commit -m ""
+git status
+git log
+""""
