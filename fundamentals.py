73fe53822c3feded13c95dc95fcca50844f1a77e
Max <maaxx.360@gmail.com>
2018-06-29 18:30:08 -0500

    Fundamentals

--- fundamentals.py
+++ fundamentals.py
@@ -64,4 +64,28 @@ git commit -m "Modified"
 git pull origin master
 git push origin master
 
+git remote add origin url
+git push -u origin master
+
+COMMON WORKFLOW
+
+git branch name
+git checkout name
+
+PUSH BRANCH TO REMOTE AFTER COMMIT
+
+git push -u origin branchname
+git branch -a
+
+git pull
+git push
+
+MERGE A BRANCH
+
+git checkout master
+git pull origin master
+git branch --merged
+git merge branchname
+git push origin master
+
 """"
